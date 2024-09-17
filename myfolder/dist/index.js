417b86b5935662e4faeca366cae0e3de9c5a2e54
Mursalin <mursalin2311@gmail.com>
2024-09-18 01:55:17 +0600

    restarting the ts yay!

--- myfolder/dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-let you = " Next level Web Developer Mursalin new ";
-console.log(you);
- 
--- myfolder/src/index.ts
+++
@@ -1,2 +0,0 @@
-let you:string = " Next level Web Developer Mursalin new ";
-console.log(you);
