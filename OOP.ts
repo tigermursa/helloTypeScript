aaec6d64d40b95a429a5993455919447ab9836f2
Mursalin <mursalin2311@gmail.com>
2025-05-06 13:56:54 +0600

    ok class 2nd also was easy nice

--- OOP.ts
+++ OOP.ts
@@ -1,3 +1,4 @@
+//! CLASS AND OBJECT
 class Animal {
   // class er properties
   public name: string;
@@ -16,3 +17,31 @@ class Animal {
 
 const dog = new Animal("German", "L3", "Gheao"); //instance
 // dog.makeSound();
+
+//! Inheritance in OOP
+
+class Animal2 {
+  name: string;
+  species: string;
+  sound: string;
+
+  constructor(name: string, species: string, sound: string) {
+    this.name = name;
+    this.species = species;
+    this.sound = sound;
+  }
+  makeSound() {
+    console.log(`says ${this.name}`);
+  }
+}
+
+class AnimalChild1 extends Animal2 {
+  extra: boolean;
+  constructor(name: string, species: string, sound: string, extra: boolean) {
+    super(name, species, sound);
+    this.extra = extra;
+  }
+  makeSound() {
+    console.log(`says ${this.name}`);
+  }
+}
